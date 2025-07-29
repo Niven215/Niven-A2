84e453f48c329b1de69b1cb31a1aee89932472bd
Niven215 <nivenmugambi@gmail.com>
2025-07-29 22:32:34 +0800

    update person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,46 @@
 public class Person {
+    // Instance variables
+    private String name;
+    private int age;
+    private String address;
+
+    // Constructor
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + ", address='" + address + "'}";
+    }
 }
+
