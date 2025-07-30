fba24b4280b6a7c8766c2316a508d0d0be4c580a
Niven215 <nivenmugambi@gmail.com>
2025-07-30 19:47:04 +0800

    Updated employee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,21 @@
-public class Employee {
-}
+public class Employee extends Person {
+    // Instance variables specific to Employee
+    private String jobTitle;
+    private String shift;
+
+    // Default constructor
+    public Employee() {
+        super("", 0, ""); // calling Person's constructor with default values
+        this.jobTitle = "";
+        this.shift = "";
+    }
+
+    // Parameterized constructor
+    public Employee(String name, int age, String address, String jobTitle, String shift) {
+        super(name, age, address); // calling Person's constructor
+        this.jobTitle = jobTitle;
+        this.shift = shift;
+    }
+
+    // Getter and Setter for jobTitle
+    public String get
