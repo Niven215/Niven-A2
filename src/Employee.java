86909e8a97cd219beb98c15ed87aa166e6d762a5
Niven215 <nivenmugambi@gmail.com>
2025-08-05 22:43:03 +0800

    update employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,21 +1,34 @@
+// Employee class that extends Person - for theme park staff
 public class Employee extends Person {
-    // Instance variables specific to Employee
+    // Attributes specific to staff
     private String jobTitle;
     private String shift;
 
     // Default constructor
     public Employee() {
-        super("", 0, ""); // calling Person's constructor with default values
+        super(); // Calls Person's default constructor
         this.jobTitle = "";
         this.shift = "";
     }
 
     // Parameterized constructor
     public Employee(String name, int age, String address, String jobTitle, String shift) {
-        super(name, age, address); // calling Person's constructor
+        super(name, age, address); // Calls Person's constructor with parameters
         this.jobTitle = jobTitle;
         this.shift = shift;
     }
 
     // Getter and Setter for jobTitle
-    public String get
+    public String getJobTitle() { return jobTitle; }
+    public void setJobTitle(String jobTitle) { this.jobTitle = jobTitle; }
+
+    // Getter and Setter for shift
+    public String getShift() { return shift; }
+    public void setShift(String shift) { this.shift = shift; }
+
+    // toString method to return full Employee details
+    @Override
+    public String toString() {
+        return super.toString() + ", Job Title: " + jobTitle + ", Shift: " + shift;
+    }
+}
